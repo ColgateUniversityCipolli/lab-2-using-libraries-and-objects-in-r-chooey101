0210385a4147caad4837a8e2d52ecd0c25f03879
Charlie Hooey <charleshooey@gmail.com>
2025-02-02 17:51:50 -0500

    Part 1 almost done

--- Lab2WORK.R
+++ Lab2WORK.R
@@ -1,17 +1,21 @@
+library(stringr)
 mus.vec<-list.dirs("MUSIC/") #Lists all files within the MUSIC folder
 num.slash<-str_count(mus.vec, "/") #Vector which counts the number of slashes present within each file's name
-album.sub<-mus.vec[num.slash == 2] #Vector holding characters of all album subdirectories
-print(album.sub)
-pres.alb<-album.sub[1]
+album.sub<-mus.vec[num.slash == 2] #Vector holding names of all album subdirectories
+for(i in 1:length(album.sub)){ #Loop which iterates through all albums of the album sub
+pres.alb<-album.sub[i]
 pres.files<-list.files(pres.alb)
 wav.files <- pres.files[str_count(pres.files, ".wav")==1]
-song.1<-wav.files[1]
-#code.to.process<-
-track.files<-paste(pres.alb , pres.files , sep="/")
+code.to.process <- c()
+for(j in 1:length(wav.files)){
+song.curr<-wav.files[j] #Obj referring to each song listed through the loop
 artist.name<-(str_split(pres.alb, "/", simplify=TRUE)[2])
 album.name<-(str_split(pres.alb, "/", simplify = TRUE)[3])
-track.name<-(str_split(song.1, "-", simplify=TRUE)[3])
+track.name<-(str_split(song.curr, "-", simplify=TRUE)[3])
 alttrack.name<-(str_sub(track.name, end=-5L))
-
 output.file<-paste(artist.name, "-", album.name, "-", alttrack.name, ".json", sep="")
-output.file
+output.command<-paste('streaming_extractor_music.exe "', song.curr, '" "', output.file, '"', sep = "")
+code.to.process <- c(code.to.process, output.command)
+}
+}
+writeLines(code.to.process, "batfile.txt")
